34c6024d3863e9771c09947e86e26b03301ee4c9
Akhilesh Khapangee <akhapangee@gmail.com>
2023-01-13 00:14:42 -0600

    Initial commit

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/HashMapToJsonConverter.java
@@ -0,0 +1,45 @@
+package com.udacity.jdnd.course3.critter.repository;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.AttributeConverter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class HashMapToJsonConverter implements AttributeConverter<Map<String, Object>, String> {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(Map<String, Object> value) {
+
+        String customerInfoJson = null;
+        try {
+            customerInfoJson = objectMapper.writeValueAsString(value);
+        } catch (final JsonProcessingException e) {
+            log.error("JSON writing error", e);
+        }
+
+        return customerInfoJson;
+    }
+
+    @Override
+    public Map<String, Object> convertToEntityAttribute(String jsonValue) {
+
+        Map<String, Object> customerInfo = null;
+        try {
+            customerInfo = objectMapper.readValue(jsonValue,
+                    new TypeReference<HashMap<String, Object>>() {
+                    });
+        } catch (final IOException e) {
+            log.error("JSON reading error", e);
+        }
+
+        return customerInfo;
+    }
+}
